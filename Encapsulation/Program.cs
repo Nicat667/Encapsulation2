cbbba923425dfe7fbc9d61c636a7defe8a4cc491
Nicat667 <nijatvo@code.edu.az>
2024-08-17 23:25:27 +0400

    files are added

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using Encapsulation;
+
+//User user = new User();
+//Console.WriteLine(user.HasDigit("sakdjkf"));
+
+//User user1 = new User();
+//Console.WriteLine(user1.HasUpper("sassdf"));
+
+//User user2 = new User();
+//Console.WriteLine(user2.HasLower("ADDEFEFl"));
+
+User user3 = new()
+{
+    username = "nicatorucovbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+    password = "nicat223"
+};
+Console.WriteLine($"{user3.username}  {user3.password}");
---
+++ Encapsulation/User.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        public string username 
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if(value.Length>=6 && value.Length<=25)
+                {
+                    _username = value;
+                }
+            } 
+        }
+        public string password 
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if(value.Length>=8 && value.Length<=25)
+                {
+                    if (HasDigit(value) && HasLower(value) && HasUpper(value))
+                    {
+                        _password = value;
+                    }
+                }
+            }
+        }
+
+        public bool HasDigit(string value)
+        {
+            bool result = false;
+            foreach (char c in value)
+            {
+                if (char.IsDigit(c))
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public bool HasUpper(string value)
+        {
+            bool result = false;
+            foreach (char c in value)
+            {
+                if (char.IsUpper(c))
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        public bool HasLower(string value)
+        {
+            bool result = false;
+            foreach (char c in value)
+            {
+                if (char.IsLower(c))
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
